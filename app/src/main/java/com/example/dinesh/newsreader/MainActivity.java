8fed5f7556d04c5ed76525f7068ae8f5a7825a2e
Dinesh99105 <dineshpandey018@gmail.com>
2019-03-23 21:10:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dinesh/newsreader/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.dinesh.newsreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+
+    ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,titles);
+
+        listView.setAdapter(arrayAdapter);
+    }
+}

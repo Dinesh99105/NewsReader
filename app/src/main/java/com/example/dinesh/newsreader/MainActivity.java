a8b536d5cd1af2494768dff9a2ed958523978576
Dinesh99105 <dineshpandey018@gmail.com>
2019-03-25 23:35:09 +0530

    added some code

--- app/src/main/java/com/example/dinesh/newsreader/MainActivity.java
+++ app/src/main/java/com/example/dinesh/newsreader/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.dinesh.newsreader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,5 +31,61 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,titles);
 
         listView.setAdapter(arrayAdapter);
+
+        DownloadTask task = new DownloadTask();
+
+        try {
+
+            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String,Void,String> {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(strings[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while(data != -1){
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                Log.i("URLContent", result);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
+
